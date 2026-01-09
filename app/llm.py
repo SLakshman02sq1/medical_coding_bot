5065216d4a286e74b9b3393fb91e1f608c482d95
SLakshman02sq1 <lakshman.s@sq1.security>
2026-01-09 05:07:10 +0000

    Additional Commits

--- app/llm.py
+++ app/llm.py
@@ -1,19 +1,22 @@
 import os
-import requests,sys
+import sys
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
-# Load environment variables
+# load .env variable
 load_dotenv()
 
+# getting api key from .env
 groq_api_key = os.getenv("GROQ_API_KEY")
 
+# if-else condition to check the availability of the api key
 if not groq_api_key:
     print("Error: GROQ_API_KEY not found")
     sys.exit(1)
 else:
     print("GROQ_API_KEY Successfully loaded")
-    
+
+# function to load the llm and set its parameters and api key
 def load_llm():
     return ChatGroq(
         model="llama-3.1-8b-instant",
@@ -22,4 +25,5 @@ def load_llm():
         api_key=groq_api_key
     )
 
-llm = load_llm()
+# assigning the function to a callable variable
+llm = load_llm() 
